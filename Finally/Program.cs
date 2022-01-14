cd7596ba4cc071ee9dbe695923509cc8c413fcd8
Alexm9002 <alexm9002@gmail.com>
2022-01-14 10:54:20 -0300

    Base Finally

---
+++ Finally/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+public class ProcessFile
+{
+    public static void Main()
+    {
+        FileStream fs = null;
+        try
+        {
+            fs = new FileStream(@"C:\temp\MariaJose.txt", FileMode.Open);
+            StreamReader sr = new StreamReader(fs);
+            string line = sr.ReadLine();
+            Console.WriteLine(line);
+        }
+        catch (DirectoryNotFoundException e) //Diretório não encontrado.
+        {
+            Console.WriteLine(e.Message);
+        }
+        finally
+        {
+            if (fs != null)
+            {
+                fs.Close();
+            }
+        }
+    }
+}
